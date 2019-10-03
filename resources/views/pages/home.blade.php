2214949d0468efa1989b1375d74f3e3bf437c890
nurmaulanimt <36754283+nurmaulanimt@users.noreply.github.com>
2019-10-03 15:45:33 +0700

    home content

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,63 @@
+@extends('layout.frontend.master')
+@section('content')
+
+	<!-- Article -->
+
+	<div class="blogs">
+		<div class="container">
+			<div class="row blogs_container">
+				<div class="col-lg-4 blog_item_col">
+					<div class="blog_item">
+						<div class="blog_background" style="background-image:url(images/1.jpg)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<span class="blog_meta">Testing Process</span>
+							<h4 class="blog_title">Choose your type testing that suits your company needs.</h4>
+							<a class="blog_more" href="#">Read more</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 blog_item_col">
+					<div class="blog_item">
+						<div class="blog_background" style="background-image:url(images/2.jpg)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<span class="blog_meta">Buy STELL</span>
+							<h4 class="blog_title">Click button bellow, and you'll find out how easy it is to buy STELL documents.</h4>
+							<a class="blog_more" href="#">Read more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Our Partner -->
+
+	<div class="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+					<div class="section_title">
+						<h2>Our Partner</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row banner_container">
+				<div class="col-lg-3">
+					<div class="blog_item">
+						<div class="banner_background" style="background-image:url(images/p3.png)"></div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="banner_item">
+						<div class="banner_background" style="background-image:url(images/p2.png)"></div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="banner_item">
+						<div class="banner_background" style="background-image:url(images/p1.png)"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
