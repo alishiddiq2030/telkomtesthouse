6baa3c4a75e0880e448d716c98a3b3d308fadcd8
nurmaulanimt <36754283+nurmaulanimt@users.noreply.github.com>
2019-10-08 19:03:12 +0700

    page profile-certification

---
+++ resources/views/pages/certification.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.frontend.master')
+@section('content')
+
+	<!-- Page Content -->
+<div class="container">
+
+  <h4 class="font-weight-light text-center text-lg-left mt-4 mb-0">Certification</h4>
+
+  <hr class="mt-2 mb-5">
+
+  <div class="row text-center text-lg-left">
+
+    <div class="col-lg-4 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/c1.jpeg" alt="">
+            <h5 align="center">1</h5>
+          </a>
+    </div>
+    <div class="col-lg-4 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/c2.jpeg" alt="">
+            <h5 align="center">Hal yang harus diperhatikan</h5>
+          </a>
+    </div>
+    <div class="col-lg-4 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/c3.jpeg" alt="">
+            <h5 align="center">Seputar Uji Fungsi</h5>
+          </a>
+    </div>
+   <div class="col-lg-4 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/c4.jpeg" alt="">
+            <h5 align="center">Sertifikat Mutu</h5>
+          </a>
+    </div>
+   <div class="col-lg-4 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/c5.jpeg" alt="">
+            <h5 align="center">SPB</h5>
+          </a>
+    </div>
+    <div class="col-lg-4 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/c6.jpeg" alt="">
+            <h5 align="center">Tarif Uji Lokasi</h5>
+          </a>
+    </div>
+  </div>
+
+</div>
+<!-- /.container -->
+@stop
