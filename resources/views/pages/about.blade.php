bbb5086d8595d0b75815e7f18c608cbb19a77ec5
nurmaulanimt <36754283+nurmaulanimt@users.noreply.github.com>
2019-10-08 18:56:03 +0700

    page profile-about

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.frontend.master')
+@section('content')
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+<div class="aboutus-area">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <div class="col-md-2 col-sm-6 col-xs-12">    
+                <div class="aboutus-image float-left hidden-sm"><img src="https://devitems.com/preview/sopnovilla/sopnovilla/img/about/1.jpg" alt=""></div>
+                </div>
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <div class="aboutus-content ">
+                    <h1 align="center">About US</h1>
+                    <h4>Lorem Ipsum</h4>
+                    <p>Lorem Ipsum is not simply random text. It has oots in a piece of classitin literature from 45 BC, making it over 2000 years old. Richard McClint professor at Hamden dney College irginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature.</p>                       
+                    </div>   
+                </div>
+            </div>    
+            </div>
+        </div>
+    </div>
+</div>
+@stop
