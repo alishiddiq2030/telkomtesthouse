e18276c27b17228c0cae9f4c512365a4c20c87fb
nurmaulanimt <36754283+nurmaulanimt@users.noreply.github.com>
2019-10-08 23:14:59 +0700

    Update contact.blade.php

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,20 +1,13 @@
 @extends('layout.frontend.master')
 @section('content')
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
-
-<div class="aboutus-area">
+<div class="contact">
     <div class="container">
         <div class="row">
             <div class="col">
-            <div class="col-md-2 col-sm-6 col-xs-12">    
-                <div class="aboutus-image float-left hidden-sm"><img src="https://devitems.com/preview/sopnovilla/sopnovilla/img/about/1.jpg" alt=""></div>
-                </div>
-            <div class="col-md-8 col-sm-6 col-xs-12">
+            <div class="col-md-12 col-sm-6 col-xs-12">
                 <div class="aboutus-content ">
-                    <h1 align="center">Contact</h1>
+                    <h2 align="center">Contact</h2>
                     <br>
                     <h4 align="center">Address</h4>
                     <p align="center">Jl. Gegerkalong Hilir, Sukarasa, <br> Sukasari, Kota Bandung, Jawa Barat 40152</p>
