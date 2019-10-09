17e4c6dd56f14a606598170375ef634499c1cff8
Ali H. Ash Shiddiq <alishiddiq2030@gmail.com>
2019-10-09 14:20:53 +0700

    halo

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,7 +10,7 @@
                 <!-- <h4>Lorem Ipsum</h4> -->
                 <img class="img-fluid img-thumbnail" src="images/slider.jpg" alt="">
                 <br>
-                <p align="center">Lorem Ipsum is not simply random text. It has oots in a piece of classitin literature from 45 BC, making it over 2000 years old. Richard McClint professor at Hamden dney College irginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature.</p>                       
+                <p align="center">Halo Lorem Ipsum is not simply random text. It has oots in a piece of classitin literature from 45 BC, making it over 2000 years old. Richard McClint professor at Hamden dney College irginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature.</p>                       
                 </div>
             </div>    
         </div>
