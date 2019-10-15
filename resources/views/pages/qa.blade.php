f500f6d421c0321c3c4a0489fd8178249c32ee0a
nurmaulanimt <36754283+nurmaulanimt@users.noreply.github.com>
2019-10-15 15:43:34 +0700

    qa

--- resources/views/pages/qa.blade.php
+++ resources/views/pages/qa.blade.php
@@ -6,15 +6,14 @@
         <div class="wizard-v3-content">
             <div class="wizard-form">
                 <div class="wizard-header">
-                    <h3>Quality Assurance Testing Process</h3>
-                    <hr>
+                    <h3 class="heading mt-5">QUALITY ASSURANCE TESTING PROCESS</h3>
                 </div>
                 <form class="form-register" action="#" method="post">
                     <div id="form-total">
                         <!-- SECTION 1 -->
                         <h2>
                             <span class="step-icon"><i align="center">1</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-text">&nbsp;&nbsp;&nbsp;&nbsp;Data<br>Registrasi</span>
                         </h2>
                         <section>
                             <div class="inner">
@@ -77,74 +76,9 @@
                         <!-- SECTION 2 -->
                         <h2>
                             <span class="step-icon"><i align="center">2</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-text">&nbsp;&nbsp;&nbsp;&nbsp;Data<br>Perusahaan</span>
                         </h2>
                         <section>
-                            <!-- <div class="inner">
-                                <div class="form-row">
-                                    <div id="radio">
-                                        <label>Companies Type :</label>
-                                        <input type="radio" name="gender" value="male" checked class="radio-1"> Male
-                                        <input type="radio" name="gender" value="female"> Female
-                                        <div class="col-sm-10">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-holder form-holder-2">
-                                        <label class="form-row-inner">
-                                            <input type="text" id="username" name="username" required>
-                                            <span class="label">Name</span>
-                                            <span class="border"></span>
-                                        </label>
-                                    </div>
-                                </div>
-
-                                <div class="form-row form-row-date">
-                                    <div class="form-holder form-holder-2">
-                                        <label for="date" class="special-label">Date of Birth*:</label>
-                                        <select name="date" id="date">
-                                            <option value="Day" disabled selected>Day</option>
-                                            <option value="16">16</option>
-                                            <option value="17">17</option>
-                                            <option value="18">18</option>
-                                            <option value="19">19</option>
-                                        </select>
-                                        <select name="month" id="month">
-                                            <option value="Month" disabled selected>Month</option>
-                                            <option value="Feb">Feb</option>
-                                            <option value="Mar">Mar</option>
-                                            <option value="Apr">Apr</option>
-                                            <option value="May">May</option>
-                                        </select>
-                                        <select name="year" id="year">
-                                            <option value="Year" disabled selected>Year</option>
-                                            <option value="2017">2017</option>
-                                            <option value="2016">2016</option>
-                                            <option value="2015">2015</option>
-                                            <option value="2014">2014</option>
-                                            <option value="2013">2013</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-holder form-holder-2">
-                                        <label class="form-row-inner">
-                                            <input type="text"  id="phone" name="phone" required>
-                                            <span class="label">Phone Number*</span>
-                                            <span class="border"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-holder form-holder-1">
-                                        <label class="form-row-inner">
-                                            <input type="text"  id="address" name="address" required>
-                                            <span class="label">Address*</span>
-                                            <span class="border"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                            </div> -->
                              <div class="inner">
                                 <div class="form-row">
                                  <label class="col-form-label col-sm-2 pt-0">Companies Type : </label>
@@ -210,10 +144,10 @@
                                 </div>
                             </div>
                         </section>
-
+                        <!-- SECTION 3 -->
                         <h2>
-                            <span class="step-icon"><i align="center">2</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-icon"><i align="center">3</i></span>
+                            <span class="step-text">&nbsp;&nbsp;&nbsp;&nbsp;Data<br>Perangkat</span>
                         </h2>
                         <section>
                              <div class="inner">
@@ -294,8 +228,8 @@
                         </section>
                         <!-- SECTION 4 -->
                         <h2>
-                            <span class="step-icon"><i align="center">1</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-icon"><i align="center">4</i></span>
+                            <span class="step-text">&nbsp;&nbsp;&nbsp;&nbsp;Data<br>Lampiran</span>
                         </h2>
                         <section>
                             <div class="inner">
@@ -314,33 +248,7 @@
                                         </label>
                                     </div>
                                 </div>
-                                 <div class="form-row form-row-date">
-                                    <div class="form-holder form-holder-2">
-                                        <label for="date" class="special-label">Date of SIUPP</label>
-                                        <select name="date" id="date">
-                                            <option value="Day" disabled selected>Day</option>
-                                            <option value="16">16</option>
-                                            <option value="17">17</option>
-                                            <option value="18">18</option>
-                                            <option value="19">19</option>
-                                        </select>
-                                        <select name="month" id="month">
-                                            <option value="Month" disabled selected>Month</option>
-                                            <option value="Feb">Feb</option>
-                                            <option value="Mar">Mar</option>
-                                            <option value="Apr">Apr</option>
-                                            <option value="May">May</option>
-                                        </select>
-                                        <select name="year" id="year">
-                                            <option value="Year" disabled selected>Year</option>
-                                            <option value="2017">2017</option>
-                                            <option value="2016">2016</option>
-                                            <option value="2015">2015</option>
-                                            <option value="2014">2014</option>
-                                            <option value="2013">2013</option>
-                                        </select>
-                                    </div>
-                                </div>
+
                                 <div class="form-row">
                                     <div class="form-holder form-holder-2">
                                          <label>SIUPP File</label>
@@ -409,53 +317,10 @@
                                 </div>
                             </div>
                         </section>
-                        <!-- <h2>
-                            <span class="step-icon"><i align="center">4</i></span>
-                            <span class="step-text">About</span>
-                        </h2>
-                        <section>
-                            <div class="inner">
-                                <h3>Confirm Details:</h3>
-                                <div class="form-row table-responsive">
-                                    <table class="table">
-                                        <tbody>
-                                            <tr class="space-row">
-                                                <th>Full Name:</th>
-                                                <td id="fullname-val"></td>
-                                            </tr>
-                                            <tr class="space-row">
-                                                <th>Email Address:</th>
-                                                <td id="email-val"></td>
-                                            </tr>
-                                            <tr class="space-row">
-                                                <th>Phone Number:</th>
-                                                <td id="phone-val"></td>
-                                            </tr>
-                                            <tr class="space-row">
-                                                <th>User:</th>
-                                                <td id="username-val"></td>
-                                            </tr>
-                                            <tr class="space-row">
-                                                <th>Gender:</th>
-                                                <td id="gender-val"></td>
-                                            </tr>
-                                            <tr class="space-row">
-                                                <th>Address:</th>
-                                                <td id="address-val"></td>
-                                            </tr>
-                                            <tr class="space-row">
-                                                <th>Card Type:</th>
-                                                <td id="pay-val">Credit Card</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </section> -->
                         <!-- SECTION 1 -->
                         <h2>
-                            <span class="step-icon"><i align="center">1</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-icon"><i align="center">5</i></span>
+                            <span class="step-text">Preview</span>
                         </h2>
                         <section>
                             <div class="inner">
@@ -500,8 +365,8 @@
                         </section>
                         <!-- SECTION 1 -->
                         <h2>
-                            <span class="step-icon"><i align="center">1</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-icon"><i align="center">6</i></span>
+                            <span class="step-text">&nbsp;&nbsp;Unggah<br>Form Testing</span>
                         </h2>
                         <section>
                             <div class="inner">
@@ -546,8 +411,8 @@
                         </section>
                         <!-- SECTION 1 -->
                         <h2>
-                            <span class="step-icon"><i align="center">1</i></span>
-                            <span class="step-text">About</span>
+                            <span class="step-icon"><i align="center">7</i></span>
+                            <span class="step-text">&nbsp;&nbsp;Selesai</span>
                         </h2>
                         <section>
                             <div class="inner">
